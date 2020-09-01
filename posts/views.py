f9fc9443a0f686b298837f21adf14585046ffc81
Alexander Lalym <alexander.lalym@gmail.com>
2020-09-02 01:19:10 +0300

    ready for review

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+
+def index(request):
+# одна строка вместо тысячи слов на SQL
+    latest = Post.objects.all()[:11]
+    # собираем тексты постов в один, разделяя новой строкой
+    return render(request, "index.html", {"posts" : latest})
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
